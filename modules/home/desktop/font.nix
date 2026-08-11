780e13956d543e817a8b6387091d0014c2c9acd4
vkabaczko <mihamar2005@gmail.com>
2026-08-11 23:47:40 +0200

    font

--- modules/home/desktop/font.nix
+++ modules/home/desktop/font.nix
@@ -1,34 +1,26 @@
 { pkgs, ... }:
 
 {
-  fonts = {
-    packages = with pkgs; [
-      noto-fonts
-      noto-fonts-cjk-sans
-      noto-fonts-color-emoji
-      liberation_ttf
-      jetbrains-mono
-    ];
+  home.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    liberation_ttf
+    jetbrains-mono
+  ];
 
-    fontconfig = {
-      enable = true;
+  fonts.fontconfig = {
+    enable = true;
 
-      defaultFonts = {
-        sansSerif = [ "Noto Sans" ];
-        serif = [ "Noto Serif" ];
-        monospace = [ "JetBrains Mono" ];
-        emoji = [ "Noto Color Emoji" ];
-      };
-
-      antialias = true;
-      hinting = {
-        enable = false;
-        style = "slight";
-      };
-      subpixel = {
-        rgba = "rgb";
-        lcdfilter = "default";
-      };
+    defaultFonts = {
+      sansSerif = [ "Noto Sans" ];
+      serif = [ "Noto Serif" ];
+      monospace = [ "JetBrains Mono" ];
+      emoji = [ "Noto Color Emoji" ];
     };
+
+    antialiasing = true;
+    hinting = "none";
+    subpixelRendering = "rgb";
   };
 }
